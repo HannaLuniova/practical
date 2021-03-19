1a003efe667570a4d4987a7642f34027cffd13c2
HannaLuniova <74614685+HannaLuniova@users.noreply.github.com>
2021-03-19 10:18:16 +0300

    end

--- js/cvet/main.js
+++
@@ -1,9 +0,0 @@
-//заданы переменные
-let page = document.querySelector('.page');
-let themeButton = document.querySelector('.theme-button');
-
-//переключает темы с тёмной на светлую и наоборот
-themeButton.onclick = function () {
-    page.classList.toggle("light-theme");
-    page.classList.toggle("dark-theme")
-}
